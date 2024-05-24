c121ae728c5740646342a6f5539ee91d07ff3d68
RonenSiv <ronensivak@gmail.com>
2024-05-24 21:06:19 +0300

    handled upload logic

--- services/client/app/components/UploadForm.tsx
+++ services/client/app/components/UploadForm.tsx
@@ -1,14 +1,93 @@
 "use client";
 
-export default function UploadForm() {
+import { ChangeEvent, useState } from "react";
+import Image from "next/image";
+import { UploadProcess } from "@/app/dashboard/upload/page";
+
+export default function UploadForm({
+  handleUpload,
+}: {
+  handleUpload: (arg: UploadProcess) => void;
+}) {
+  const [thumbnail, setThumbnail] = useState<string | null>(null);
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && file.type.startsWith("video/")) {
+      generateThumbnail(file);
+      await uploadFile(file);
+    }
+  };
+
+  const generateThumbnail = (file: File) => {
+    const videoElement = document.createElement("video");
+    const canvasElement = document.createElement("canvas");
+    const context = canvasElement.getContext("2d");
+
+    videoElement.src = URL.createObjectURL(file);
+    videoElement.currentTime = 1;
+
+    videoElement.onloadeddata = () => {
+      canvasElement.width = videoElement.videoWidth;
+      canvasElement.height = videoElement.videoHeight;
+      context?.drawImage(
+        videoElement,
+        0,
+        0,
+        canvasElement.width,
+        canvasElement.height,
+      );
+      setThumbnail(canvasElement.toDataURL());
+    };
+  };
+
+  const uploadFile = async (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        handleUpload(UploadProcess.UPLOAD_SUCCEED);
+      } else {
+        handleUpload(UploadProcess.UPLOAD_FAILED);
+      }
+    } catch (error) {
+      console.error("Error uploading video:", error);
+      alert("Error uploading video.");
+    }
+  };
+
   return (
-    <form className="inline-flex flex-col gap-4">
-      <label>
-        <div className="text-[#2CA15D] hover:cursor-pointer underline">
-          Upload a file
+    <div className="flex flex-row gap-4">
+      <form className="inline-flex flex-col justify-center justify-items-center gap-4">
+        <label>
+          <div className="text-[#2CA15D] hover:cursor-pointer underline">
+            Upload a file
+          </div>
+          <input
+            className="hidden"
+            type="file"
+            name="file"
+            accept="video/*"
+            onChange={handleFileChange}
+          />
+        </label>
+      </form>
+      {thumbnail && (
+        <div>
+          <Image
+            src={thumbnail}
+            alt="Video thumbnail"
+            width={150}
+            height={150}
+            className="max-w-full h-auto"
+          />
         </div>
-        <input className="hidden" type="file" name="file" />
-      </label>
-    </form>
+      )}
+    </div>
   );
 }
--- services/client/app/dashboard/upload/page.tsx
+++ services/client/app/dashboard/upload/page.tsx
@@ -1,17 +1,45 @@
+"use client";
 import { Card } from "@/app/components/Card";
 import Image from "next/image";
 import UploadForm from "@/app/components/UploadForm";
 import { LinkSubmission } from "@/app/components/LinkSubmission";
+import { useState } from "react";
 
-export default async function Upload() {
+export enum UploadProcess {
+  DIDNT_UPLOAD,
+  UPLOAD_SUCCEED,
+  UPLOAD_FAILED,
+}
+
+export default function Upload() {
+  const [didUpload, setDidUpload] = useState<UploadProcess>(
+    UploadProcess.DIDNT_UPLOAD,
+  );
+  const handleUpload = (succeed: UploadProcess) => {
+    console.log(succeed);
+    setDidUpload(succeed);
+  };
+
+  const showUploadProcess = () => {
+    switch (didUpload) {
+      case UploadProcess.DIDNT_UPLOAD:
+        return null;
+      case UploadProcess.UPLOAD_SUCCEED:
+        return <div className="text-green-300">Upload succeed!</div>;
+      case UploadProcess.UPLOAD_FAILED:
+        return <div className="text-red-500">Upload failed!</div>;
+    }
+  };
+
   return (
     <Card
       title="Upload Video"
       titleColor="#2CA15D"
-      className="justify-content-center text-center"
+      className="justify-content-center text-center w-screen"
       maxWidth="5xl"
     >
-      <Card className="bg-[#EFFFF6] w-screen" maxWidth="4xl">
+      <Card className="bg-[#EFFFF6]" maxWidth="5xl">
+        {showUploadProcess()}
         <div className="flex flex-col justify-items-center py-10">
           <div className="flex justify-center items-center">
             <div>
@@ -22,12 +50,10 @@ export default async function Upload() {
                 height="75"
               />
             </div>
-            <div className="text-gray-400">
-              Drag a video here or
-              <span className="ml-1">
-                <UploadForm />
-              </span>
-            </div>
+            <div className="text-gray-400">Drag a video here or</div>
+            <span className="ml-1">
+              <UploadForm handleUpload={handleUpload} />
+            </span>
           </div>
           <div className="flex items-center justify-center py-16">
             <hr className="w-full border-t-1 border-gray-300" />
