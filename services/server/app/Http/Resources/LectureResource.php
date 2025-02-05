7c12872d6ce98976ed6afa63004bf3d3b2a45de4
Ofir Goldberg <74971232+ofirgoldofir@users.noreply.github.com.>
2025-02-05 15:56:37 +0200

    fix mindmap returning

--- services/server/app/Http/Resources/LectureResource.php
+++ services/server/app/Http/Resources/LectureResource.php
@@ -24,7 +24,7 @@ class LectureResource extends JsonResource
             'summary' => $this->summary,
             'quiz' => new QuizResource($this->quiz),
             'chat' => new ChatResource($this->chat),
-            'mind_map' => $this->mind_map
+            'mind_map' => json_decode($this->mind_map),
         ];
     }
 }
